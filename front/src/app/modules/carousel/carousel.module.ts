a573a78b5fbfe2cf71d5ffc85e8d3fe94e094be4
Sebastian Gonzalez <gabrielsebastian@gmail.com>
2022-09-17 17:01:07 -0300

    elimino carpetas

---
+++ front/src/app/modules/carousel/carousel.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CarouselRoutingModule } from './carousel-routing.module';
+import { CarouselComponent } from './carousel/carousel.component';
+
+
+@NgModule({
+  declarations: [
+    CarouselComponent
+  ],
+  imports: [
+    CommonModule,
+    CarouselRoutingModule
+  ]
+})
+export class CarouselModule { }
