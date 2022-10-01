ac4324922f855d354f057731282b1e1fc6c6bf5e
Diana Cabrera <lic.cabreradiana@gmail.com>
2022-10-01 20:51:59 -0300

    modificaciones

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,47 +4,47 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    canActivate: [NotLoggedInGuard],
+    canActivate: [],
     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
   },
   {
     path: 'navbar',
-    canActivate: [NotLoggedInGuard],
+    canActivate: [],
     loadChildren: () => import('./modules/navbar/navbar.module').then(m => m.NavbarModule),
   },
   {
     path: 'home',
-    canActivate: [NotLoggedInGuard],
+    canActivate: [],
     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
   },
   {
     path: 'gastronomia',
-    canActivate: [NotLoggedInGuard],
+    canActivate: [],
     loadChildren: () => import('./modules/gastronomia/gastronomia.module').then(m => m.GastronomiaModule),
   },
   {
     path: 'atractivos',
-    canActivate: [NotLoggedInGuard],
+    canActivate: [],
     loadChildren: () => import('./modules/atractivos/atractivos.module').then(m => m.AtractivosModule),
   },
     {
       path: 'alojamientos',
-      canActivate: [NotLoggedInGuard],
+      canActivate: [],
       loadChildren: () => import('./modules/alojamientos/alojamientos.module').then(m => m.AlojamientosModule),
     },
     {
       path: 'agencias',
-      canActivate: [NotLoggedInGuard],
+      canActivate: [],
       loadChildren: () => import('./modules/agencias/agencias.module').then(m => m.AgenciasModule),
     },
     {
       path: 'actividades',
-      canActivate: [NotLoggedInGuard],
+      canActivate: [],
       loadChildren: () => import('./modules/actividades/actividades.module').then(m => m.ActividadesModule),
     },
     {
       path: 'footer',
-      canActivate: [NotLoggedInGuard],
+      canActivate: [],
       loadChildren: () => import('./modules/footer/footer.module').then(m => m.FooterModule),
     },
     {path:'**',
