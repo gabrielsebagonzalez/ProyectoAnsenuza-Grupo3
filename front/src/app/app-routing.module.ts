00368a52d864c11d1016a8efdaf2e53016bf4310
Diana Cabrera <lic.cabreradiana@gmail.com>
2022-10-05 17:48:40 -0300

    addLoggin

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LoggedInGuard } from './core/guards/login/loggedin/loggedin.guard';
+import { NotLoggedInGuard } from './core/guards/login/notloggedin/notloggedin.guard';
+
 const routes: Routes = [
   {
     path: '',
